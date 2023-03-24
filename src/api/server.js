0fa54305d6cecf1bebfb9e7e62fa47278b356cbc
luisf4 <luismarcelino.dev@gmail.com>
2023-03-24 14:09:44 -0300

    fuck auth this will not have authentication and no database, im migrating it to just a .json file in the src/api/data directory >:c

--- src/api/server.js
+++ src/api/server.js
@@ -1,53 +1,82 @@
 const express = require('express');
-const admin = require('firebase-admin');
+const fs = require('fs');
+const path = require('path');
 
-const jwt = require('jsonwebtoken');
-
+// Set express
 const app = express();
+// Set json as default 
 app.use(express.json());
 
-const serviceAccount = require('./firebase.json');
+// Defines the path to the json file 
+const tasksFile = path.join(__dirname, 'data/tasks.json');
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+// Function to read the tasks.json file
+function readTasksFile() {
+  const data = fs.readFileSync(tasksFile);
+  return JSON.parse(data);
+}
 
-const JWT_SECRET = '';
+// Function to write to the tasks.json  file 
+function writeTasksFile(tasks) {
+  fs.writeFileSync(tasksFile, JSON.stringify(tasks));
+}
 
-// Register endpoint
-app.post('/api/register', async (req, res) => {
-  const { email, password } = req.body;
+// Create a new task
+app.post('/api/tasks', (req, res) => {
+  const { name } = req.body;
 
-  try {
-    const userRecord = await admin.auth().createUser({
-      email,
-      password,
-    });
-    const token = jwt.sign({ userId: userRecord.uid }, JWT_SECRET);
-    res.json({ message: 'User registered successfully.', token });
-  } catch (error) {
-    res.status(500).json({ error: 'Failed to create user.' });
+  if (!name) {
+    return res.status(400).json({ error: 'Name is required.' });
   }
+
+  const tasks = readTasksFile();
+  const newTask = { id: Date.now().toString(), name };
+  tasks.push(newTask);
+  writeTasksFile(tasks);
+
+  res.json(newTask);
 });
 
-// Login endpoint
-app.post('/api/login', async (req, res) => {
-  const { email, password } = req.body;
+// Update an existing task
+app.put('/api/tasks/:id', (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
 
-  try {
-    console.log(email, password);
-    // Try to login 
-    await admin.auth().signInWithEmailAndPassword(email, password);
-    const userRecord = await admin.auth().getUserByEmail(email);
-    const token = jwt.sign({ userId: userRecord.uid }, JWT_SECRET);
-    res.json({ message: 'Login successful.', token });
-  } catch (error) {
-    console.log(error);
-
-    res.status(401).json({ error: 'Invalid email or password.' });
+  if (!name) {
+    return res.status(400).json({ error: 'Name is required.' });
   }
+
+  const tasks = readTasksFile();
+  const taskIndex = tasks.findIndex((task) => task.id === id);
+
+  if (taskIndex === -1) {
+    return res.status(404).json({ error: 'Task not found.' });
+  }
+
+  tasks[taskIndex].name = name;
+  writeTasksFile(tasks);
+
+  res.json(tasks[taskIndex]);
 });
 
+// Delete a task
+app.delete('/api/tasks/:id', (req, res) => {
+  const { id } = req.params;
+
+  const tasks = readTasksFile();
+  const taskIndex = tasks.findIndex((task) => task.id === id);
+
+  if (taskIndex === -1) {
+    return res.status(404).json({ error: 'Task not found.' });
+  }
+
+  tasks.splice(taskIndex, 1);
+  writeTasksFile(tasks);
+
+  res.json({ message: 'Task deleted successfully.' });
+});
+
+// Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
